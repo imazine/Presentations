2069d3f107e7384785e5e4a640a0c653332ba737
iMaZiNe <imazine@me.com>
2016-08-11 17:39:27 +0900

    Change development evironment
    Add simple web server with live-reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,14 @@
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload');
+    webserver = require('gulp-webserver');
 
-gulp.task('reload', function () {
-    gulp.src('*.*')
-    .pipe(livereload());
+gulp.task('connect', function () {
+    gulp.src('./')
+    .pipe(webserver({
+        livereload: true,
+        directoryListring: true,
+        open: true
+    }));
 });
 
-gulp.task('watch', function () {
-    livereload.listen();
-
-    gulp.watch('**/*.css', function (file) {
-        livereload.changed(file);
-    });
-
-    gulp.watch('**/*.md', function (file) {
-        livereload.changed(file);
-    });
-
-    gulp.watch('**/*.html', function (file) {
-        livereload.changed(file);
-    });
-});
+gulp.task('default', ['connect']);
 
